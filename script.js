6742be78351861c7ec9e7ce9104bc919c6433250
shivasaiparsha <112009608+shivasaiparsha@users.noreply.github.com>
2024-03-15 13:49:43 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
 function getFormvalue() {
     //Write your code here
-
+    let fname=document.getElementById('fname').value;
+	let lname=document.getElementById('lname').value;
+	alert(fname+" "+lname);
 }
